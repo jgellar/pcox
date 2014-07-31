f54a2128b20899c8bf813d3fde9946f708c429f4
jgellar <jgellar1@jhu.edu>
2014-07-31 11:10:20 -0400

    Added new beta functions for simulations

--- R/makeBetaMat.R
+++ R/makeBetaMat.R
@@ -1,4 +1,3 @@
-
 makeBetaMat <- function(J=101, bfunc=genBeta1, ...) {
   grid <- do.call("rbind", sapply(0:(J-1), function(t) {
     s <- 0:t
@@ -17,4 +16,21 @@ genBeta1 <- function(t,Ti,scale=NULL) {
   # Proportion-based, no interaction
   10*ifelse(Ti==0, 0, (t/Ti)-.5)
 }
-
+genBeta2 <- function(t,Ti,scale=NULL) {
+  # Proportion-based, linear interaction
+  if (is.null(scale)) {scale <- max(Ti)}
+  10*ifelse(Ti==0, 0, (1-2*Ti/scale)*(0.5-4*(t/Ti-.5)^2))
+}
+genBeta3 <- function(t,Ti,scale=NULL) {
+  # Lag-based, logistic function
+  u <- (Ti-t)
+  5 - u/10
+}
+genBeta4 <- function(t,Ti,scale=NULL) {
+  # Lag-based, logistic function
+  u <- (Ti-t)
+  sin(2*pi*Ti/scale)*(5 - u/10)
+}
+genBeta5 <- function(s,t,lag=3) {
+  ifelse((t-s)==lag, 5, 0)
+}
