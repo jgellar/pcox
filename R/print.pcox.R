350b77187bcc5c75e21d6c546a4c716d4389f9f9
jgellar <jongellar@gmail.com>
2015-07-10 15:11:23 -0400

    Some updates to print and summary functions, as well as testing files.

--- R/print.pcox.R
+++ R/print.pcox.R
@@ -1,14 +1,23 @@
 #' @export
-print.pcox <- function(object, ...) {
-  object$call <- object$pcox$call
-  #names(object$pterms) <- c("A", "B")
-  class(object) <- class(object)[-1]
-  print(object, ...)
+print.pcox <- function(x, ...) {
+  x$call <- x$pcox$call
+  names(x$pterms)[(x$pterms==1)] <- sapply(x$pcox$smooth, function(sm) sm$label)
+  #names(x$pterms) <- c("A", "B")
+  class(x) <- class(x)[-1]
+  print(x, ...)
+  if (any(x$pterms==1))
+    cat("\nNOTE: smooth pcox coefficients may be extracted with coef()\n")
 }
 
 #' @export
 summary.pcox <- function(object, ...) {
+  if (any(object$pterms==1))
+    cat("NOTE: smooth pcox coefficients may be extracted with coef()\n\n")
+  
   object$call <- object$pcox$call
+  names(object$pterms)[(object$pterms==1)] <-
+    sapply(object$pcox$smooth, function(sm) sm$label)
   class(object) <- class(object)[-1]
   summary(object, ...)
+  
 }
--- Testing/pcox_stable.R
+++ Testing/pcox_stable.R
@@ -13,7 +13,7 @@ library(RColorBrewer)
 
 # Take these out later?
 #library(pryr)
-#library(reshape2)
+library(reshape2)
 
 
 
@@ -29,6 +29,19 @@ sind <- seq(0,1,length=J)
 X <- genX(N, sind)
 K <- 100
 Z <- genX(N, seq(0,1,length=K))
+L <- 100
+
+plotMe <- function(est, lims=range(est$value)) {
+  ggplot(est, aes(s, t)) + 
+    geom_tile(aes(fill=value, colour=value)) +
+    theme_bw() +
+    scale_fill_gradientn(name="", limits=lims,
+                         colours=rev(brewer.pal(11,"Spectral"))) +
+    scale_colour_gradientn(name="", limits=lims,
+                           colours=rev(brewer.pal(11,"Spectral"))) +
+    scale_y_continuous(expand = c(0,0)) +
+    scale_x_continuous(expand = c(0,0))
+}
 
 
 ###########################
@@ -43,10 +56,13 @@ est1.1 <- coef(fit1.1)
 pre1.1a <- predict(fit1.1)
 pre1.1b <- predict(fit1.1, newdata=dat1.1)
 range(pre1.1a - pre1.1b)
+fit1.1
 
 time1 <- dat1.1$time
 evnt1 <- dat1.1$event
 fit1.2 <- pcox(Surv(time1, evnt1) ~ x + male, data=dat1.1)
+fit1.2
+summary(fit1.2)
 
 
 ##################
@@ -58,6 +74,8 @@ eta2.1 <- matrix(sin(x) + .75*male, nrow=N, ncol=J)
 dat2.1 <- simTVSurv(eta2.1, data.frame(x=x, male=male))
 fit2.1 <- pcox(Surv(time, event) ~ p(x, linear=FALSE, k=8) +
                  male, data=dat2.1)
+fit2.1
+summary(fit2.1)
 est2.1a <- coef(fit2.1)
 est2.1b <- drop(fit2.1$pcox$t.funcs[[1]](est2.1a["x"]) %*%
                   fit2.1$coefficients[1:7])
@@ -84,6 +102,8 @@ ggplot(est2.2a, aes(t, value)) + geom_line(colour="red", size=2) +
 pre2.2a <- predict(fit2.2)
 pre2.2b <- predict(fit2.2, newdata=dat2.2, stimes=dat2.2$time)
 range(pre2.2a - pre2.2b, na.rm=T) # Should be 0
+fit2.2
+summary(fit2.2)
 
 # Two time-varying terms
 eta2.3 <- matrix(sin(2*pi*(1:J)/J) %x% z, nrow=N, ncol=J) +
@@ -91,6 +111,8 @@ eta2.3 <- matrix(sin(2*pi*(1:J)/J) %x% z, nrow=N, ncol=J) +
 dat2.3 <- simTVSurv(eta2.3, data.frame(x=z, male=male))
 fit2.3 <- pcox(Surv(time, event) ~ p(x, linear=TRUE, tv=T) +
                  p(male, linear=T, tv=T), data=dat2.3)
+fit2.3
+summary(fit2.3)
 est2.3a <- coef(fit2.3)
 est2.3b <- coef(fit2.3, select=2)
 p1 <- ggplot(est2.3a, aes(t, value)) + geom_line(colour="red", size=2) +
@@ -123,6 +145,50 @@ pre3.1a <- predict(fit3.1)
 pre3.1b <- predict(fit3.1, newdata=dat3.1)
 range(pre3.1a - pre3.1b, na.rm=T) # Should be 0
 
+# TIME-VARYING BASELINE FUNCTION
+# \beta(s,t) = 5*sin(2*pi*s + pi*t/100)
+# \eta_i(t) = \int X_i(s)\beta(s,t)ds + .75*male_i
+
+beta3.2 <- expand.grid(s=sind, t=(0:L))
+beta3.2$value <- 5*sin(2*pi*beta3.2$s + pi*beta3.2$t/100)
+#*cos(2*pi*beta3.2$t/100)
+beta3.2mat <- acast(beta3.2, t ~ s)
+eta3.2 <- t(apply(X, 1, function(x.i) {
+  x.i %*% t(beta3.2mat)/J
+})) + .75*matrix(male, nrow=N, ncol=(L+1))
+dat3.2 <- simTVSurv(eta3.2)
+dat3.2$myX <- X
+dat3.2$male <- male
+fit3.2 <- pcox(Surv(time,event) ~ bf(myX, bs="ps", basistype="te", sind=sind,
+                                     tv=TRUE) + male, data=dat3.2)
+est3.2 <- coef(fit3.2)
+p3.2a <- plotMe(beta3.2, c(-6,6))
+p3.2b <- plotMe(est3.2,  c(-6,6))
+p3.2c <- arrangeGrob(p3.2a, p3.2b, nrow=1)
+p3.2c
+
+# ADDITIVE BASELINE (here I'm using t instead of x just so plotMe works)
+beta3.3 <- expand.grid(t=seq(min(X), max(X), length=J), s=sind)
+beta3.3$value <- 20*sin(2*pi*beta3.3$s + pi*beta3.3$t/20)
+beta3.3mat <- acast(beta3.3, t ~ s)
+eta3.3 <- apply(X, 1, function(x.i) {
+  mean(20*sin(2*pi*sind + pi*x.i/20))
+}) + .75*male
+dat3.3 <- simTVSurv(matrix(eta3.3, nrow=N, ncol=L))
+dat3.3$myX <- X
+dat3.3$male <- male
+fit3.3 <- pcox(Surv(time,event) ~ bf(myX, bs="ps", basistype="te", sind=sind,
+                                     linear=FALSE) + male, data=dat3.3)
+est3.3 <- coef(fit3.3)
+names(est3.3)[2] <- "t"
+p3.3a <- plotMe(beta3.3, c(-30,30))
+p3.3b <- plotMe(est3.3,  c(-30,30))
+p3.3c <- arrangeGrob(p3.3a, p3.3b, nrow=1)
+p3.3c
+
+
+
+
 
 ###################
 # Concurrent TVCs #
@@ -136,6 +202,8 @@ est4.1 <- coef(fit4.1)
 pre4.1a <- predict(fit4.1)
 pre4.1b <- predict(fit4.1, newdata=dat4.1, stimes=dat4.1$time)
 range(pre4.1a - pre4.1b, na.rm=T) # Should be 0
+fit4.1
+summary(fit4.1)
 
 
 # Time-varying version
@@ -144,6 +212,8 @@ dat4.2 <- simTVSurv(eta4.2, data.frame(myX=I(X), male=male))
 fit4.2 <- pcox(Surv(time,event) ~ male + cf(myX, tv=TRUE,
                                             sind = (1:ncol(dat4.2$myX))),
                data=dat4.2)
+fit4.2
+summary(fit4.2)
 est4.2a <- coef(fit4.2)
 est4.2b <- mgcv::PredictMat(fit4.2$pcox$smooth[[1]],
                       data=data.frame(t=est4.2a$t, myX.t=1)) %*% 
@@ -162,6 +232,8 @@ eta4.3 <- 1.5*cbind(matrix(0, nrow=N, ncol=lag), X[, 1:(J-lag)]) +
 dat4.3 <- simTVSurv(eta4.3, data.frame(myX=I(X), male=male))
 fit4.3 <- pcox(Surv(time,event) ~ male + cf(myX, sind = (1:ncol(dat4.3$myX)), lag=lag),
                data=dat4.3)
+fit4.3
+summary(fit4.3)
 est4.3 <- coef(fit4.3)
 pre4.3a <- predict(fit4.3)
 pre4.3b <- predict(fit4.3, newdata=dat4.3, stimes=dat4.3$time)
@@ -172,17 +244,6 @@ range(pre4.3a - pre4.3b, na.rm=T) # Should be 0
 # Historical TVCs #
 ###################
 
-plotMe <- function(est, lims=range(est$value)) {
-  ggplot(est, aes(s, t)) + 
-    geom_tile(aes(fill=value, colour=value)) +
-    theme_bw() +
-    scale_fill_gradientn(name="", limits=lims,
-                         colours=rev(brewer.pal(11,"Spectral"))) +
-    scale_colour_gradientn(name="", limits=lims,
-                           colours=rev(brewer.pal(11,"Spectral"))) +
-    scale_y_continuous(expand = c(0,0)) +
-    scale_x_continuous(expand = c(0,0))
-}
 
 beta <- makeBetaMat(K, genBeta1)
 eta5.1  <- sapply(1:K, function(k) {
@@ -206,6 +267,8 @@ fit5.7 <- pcox(Surv(time,event) ~ male + hf(myX, sind = sinds, transform="quadra
                                             bs="ps"), data=dat5.1)
 fit5.8 <- pcox(Surv(time,event) ~ male + hf(myX, sind = sinds, transform="noInteraction",
                                             bs="ps"), data=dat5.1)
+fit5.1
+summary(fit5.1)
 plotMe(coef(fit5.1), c(-6,6))
 plotMe(coef(fit5.2), c(-6,6))
 plotMe(coef(fit5.3), c(-6,6))
